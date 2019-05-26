6c6ea677d5d12b2fdf4195dd6cc25dd11f6a25dc
Noriko Abe <code.norikoabe@gmail.com>
2019-05-26 13:13:50 +0000

    課題提出

---
+++ test.rb
@@ -0,0 +1,19 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({
+  DocumentRoot:   'https://rubyonrails-noriko96.c9users.io/',
+  BindAddress:    "0.0.0.0",
+  Port:           8080,
+  CGIInterpreter: '/usr/local/rvm/rubies/ruby-2.4.0/bin/ruby'
+})
+
+# WEBrick::HTTPServlet::FileHandlerをWEBrick::HTTPServlet::ERBHandlerに変更する
+# 'test.html'を'test.html.erb'に変更する
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+# この一行を追記
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+# この一行を追記
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.start
